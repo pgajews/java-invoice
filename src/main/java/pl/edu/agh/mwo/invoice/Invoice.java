791e4f4c96cfcad892a3d097551ddbaab457206d
Piotr Gajewski <extern.Piotr.Gajewski@itacsoftware.com>
2021-01-17 11:23:54 +0100

    Fix Invoice class regarding to tests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
@@ -8,23 +9,40 @@ import pl.edu.agh.mwo.invoice.product.Product;
 public class Invoice {
     private Collection<Product> products;
 
+    public Invoice() {
+        this.products = new ArrayList<>();
+    }
+
     public void addProduct(Product product) {
-        // TODO: implement
+        this.products.add(product);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        if (quantity < 1) {
+            throw new IllegalArgumentException("Quantity can't be less then 1");
+        }
+        int i=0;
+        while (i < quantity) {
+            addProduct(product);
+            i++;
+        }
     }
 
     public BigDecimal getSubtotal() {
-        return null;
+        return this.products.stream()
+            .map(p -> p.getPrice())
+            .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
     public BigDecimal getTax() {
-        return null;
+        return this.products.stream()
+            .map(p -> p.getPrice().multiply(p.getTaxPercent()))
+            .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
     public BigDecimal getTotal() {
-        return null;
+        return this.products.stream()
+            .map(p -> p.getPriceWithTax())
+            .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
