7b6d45cf204ed036b6d23f015617366ffef9fb01
Piotr Gajewski <extern.Piotr.Gajewski@itacsoftware.com>
2021-01-17 10:35:43 +0100

    Fix Product class regarding to tests

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,24 +10,41 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+        verifyName(name);
+        verifyPrice(price);
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
     }
 
+    private void verifyName(String name) {
+        if (name == null || name.equals("")) {
+            throw new IllegalArgumentException("Name can't be null or empty");
+        }
+    }
+
+    private void verifyPrice(BigDecimal price) {
+        if (price == null) {
+            throw new IllegalArgumentException("Price can't be null");
+        }
+        if (price.signum() < 0) {
+            throw new IllegalArgumentException("Price can't be negative");
+        }
+    }
+
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public BigDecimal getPrice() {
-        return null;
+        return this.price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+        return this.taxPercent;
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+        return this.price.add(this.price.multiply(this.taxPercent));
     }
 }
